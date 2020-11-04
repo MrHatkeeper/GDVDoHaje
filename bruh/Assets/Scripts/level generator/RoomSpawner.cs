eb867d9b8a8d1bf599ffc78be8670352303dd6ff
MrHatkeeper <58586406+MrHatkeeper@users.noreply.github.com>
2020-11-04 13:07:50 +0100

    základ levelGenereace

---
+++ bruh/Assets/Scripts/level generator/RoomSpawner.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomSpawner : MonoBehaviour
+{
+    public int openingDirection;
+
+    private LevelTemplates templates;
+
+    void Strart()
+    {
+        templates = GameObject.FindGameObjectWithTag("Rooms").GetComponent<LevelTemplates>();
+    }
+
+    void Update()
+    {
+        if(openingDirection == 1)
+        {
+
+        }   
+        else if(openingDirection == 2)
+        {
+
+        }
+        else if (openingDirection == 3)
+        {
+
+        }
+        else if (openingDirection == 4)
+        {
+
+        }
+    }
+}
